2e4ea4e5cb50a057d90b451bf489dc9cc09c5569
rauvctg2015 <rauvctg@gmail.com>
2015-02-20 13:56:39 -0500

    WebVersion of Test
    This is The Web version of a test for Junior Dev for CodeTag (.Net, C#,
    WebForm, jquery (if you want) , Others)

---
+++ testJuniorDev/testJuniorDev/index.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="testJuniorDev.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>TestJunorDev</title>
+</head>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+<script src="http://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+
+<script src="js/jquery.js" type="text/javascript"></script>
+     <script type="text/javascript">
+         function openDialog() {
+             $("#divDialogForm").dialog({
+                 modal: true,
+                 width: "20%",
+                 autoOpen: false,
+                 buttons: {
+                     "OK": function () {
+                         $(this).dialog('close');
+                     }
+                 }
+             });
+             $("#divDialogForm").dialog('open');
+             $('#divDialogForm').parent().appendTo($("form:first"));
+         }
+</script>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblTitle" runat="server" Text="Choose a File (txt) : "></asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server" /><br /><br />
+    
+    &nbsp;<asp:Button ID="btnUpload" runat="server" onclick="btnUpload_Click" 
+            Text="Upload File" />
+    
+        <br />
+        <br />
+        FileName:
+        <asp:Label ID="lblFileName" runat="server"></asp:Label><br /><br />
+        <br /><br />
+    
+    <div id="divDialogForm" style="display:;">
+    <asp:Label ID="lblTitleContent" runat="server" Text="File Content"></asp:Label><br /><br />
+        <asp:TextBox TextMode="MultiLine" ID="txtContent"  ReadOnly="true"
+            runat="server" Height="174px" Width="274px"></asp:TextBox>
+    </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ testJuniorDev/testJuniorDev/index.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace testJuniorDev
+{
+    public partial class index : System.Web.UI.Page
+    {
+        FileContent f = new FileContent();
+        List<string> array = new List<string>();
+        string resulString = "1,6,11" + Environment.NewLine + Environment.NewLine + "2,7,12" + Environment.NewLine + Environment.NewLine + "3,8,13" + Environment.NewLine + Environment.NewLine + "4,9,14";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void validateUpload()
+        {
+            try
+            {
+                string ext = System.IO.Path.GetExtension(this.FileUpload1.PostedFile.FileName);
+                
+
+                if (ext != ".txt")
+                {
+                    ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "OpenDialog", "javascript:alert('Error, file extension no valid !!');", true);
+                }
+
+                if (FileUpload1.FileName != null && ext == ".txt")
+                {
+                    this.lblFileName.Text = FileUpload1.FileName.Replace(".txt", "");
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "OpenDialog", "javascript:alert('File dont have a Name !!');", true);
+                }
+
+                byte[] content = this.FileUpload1.FileBytes;
+                
+                f.Content = System.Text.Encoding.UTF8.GetString(content);
+                array.Add(f.Content);
+
+                if (array.Count >= 1)
+                {
+                    this.txtContent.Text = array.FirstOrDefault();
+                    string fileName = System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    string dirUpload = Server.MapPath("~/Uploads/");
+
+                    FileUpload1.PostedFile.SaveAs(dirUpload + fileName);
+                    System.IO.File.WriteAllText(dirUpload + fileName, string.Empty);
+                    System.IO.File.WriteAllText(dirUpload + fileName, resulString);
+                    string content2 = System.IO.File.ReadAllText(dirUpload + fileName);
+                    this.txtContent.Text = content2;
+                    ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "OpenDialog", "javascript:openDialog();", true);                 
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "OpenDialog", "javascript:alert('This file is Empty !!');", true);
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            this.validateUpload();
+        }
+
+    }
+
+    public class FileContent
+    {
+        private string _content;
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+    }
+}
